0df177c35e92c03229bd0d0894be21e3d9f3b113
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-09-13 17:58:00 +0530

    levelorder, preorder, postorder

--- tree/main.py
+++ tree/main.py
@@ -1,55 +1,129 @@
-class node:
-    def __init__(self,value, left=None ,right=None):
+# class node:
+#     def __init__(self,value, left=None ,right=None):
+#         self.value = value
+#         self.left = left
+#         self.right = right
+    
+# #revursive funtion to build the tree
+# def buildNode():
+#     print("Enter the data")
+#     data = int(input())
+
+#     if data==-1:
+#         return
+#     root = node(data)
+
+#     print("enter data for left of ",data)
+#     root.left = buildNode()
+#     print("Enter data for rigth of ",data)
+#     root.right = buildNode()
+#     return root
+
+
+# #level order traversal
+# def levelOrderTraversal(root):
+#     q = []
+#     q.append(root)
+#     q.append(None)
+
+#     while (len(q)!=0):
+#         temp = q[-1]
+#         q.pop()
+
+#         if (temp == None):
+#             print()
+#             if (len(q)!=0):
+#                 q.append(None)
+#         else:
+#             print(temp.data)
+#             if temp.left:
+#                 q.append(temp.left)
+            
+#             if temp.right:
+#                 q.append(temp.right)
+
+
+
+class Node:
+    def __init__(self, value, left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
-    
-#revursive funtion to build the tree
+
+# Recursive function to build the tree
 def buildNode():
-    print("Enter the data")
+    print("Enter the data:")
     data = int(input())
+    
+    if data == -1:
+        return None
 
-    if data==-1:
-        return
-    root = node(data)
+    root = Node(data)
 
-    print("enter data for left of ",data)
+    print(f"Enter left child of {data}:")
     root.left = buildNode()
-    print("Enter data for rigth of ",data)
+
+    print(f"Enter right child of {data}:")
     root.right = buildNode()
+
     return root
 
+# Level-order traversal (BFS)
+def levelOrderTraversal(root):
+    if not root:
+        return
 
-#level order traversal
-# void levelOrderTraversal(node* root) {
-# queue<node*> q;
-# q- push (root);
-# q- push (NULL*;
-# while(!q empty)) {
-# node* temp = q. front);
-# q- pop();
-# if(temp == NULL) {
-# //purana level
-# complete traverse ho chuka hai
-# cout
-# < endl;
-# if(!q-empty)) {
-# //queue still has some child ndoes
-# q - push (NULL) ;
-# ｝
-# ｝
-# elset
-# cout « temp →> data «< " ";
-# if (temp ->left) {
-# q- push (temp -> left);
-# if(temp
-# →>right) {
-# q. push ( temp right);
-# ｝
-# }
-# ｝
-newNode = node(10)
+    q = []
+    q.append(root)
+    q.append(None)  # Using `None` as a level separator
+
+    while len(q) > 0:
+        temp = q.pop(0)  # Dequeue the front of the queue
+
+        if temp is None:
+            print()  # New line for a new level
+            if len(q) > 0:
+                q.append(None)  # Append `None` to mark the end of the next level
+        else:
+            print(temp.value, end=" ")  # Print current node value
+            if temp.left:
+                q.append(temp.left)  # Enqueue left child
+            if temp.right:
+                q.append(temp.right)  # Enqueue right child
+
+def inOrder(root):
+    if root==None:
+        return 
+    inOrder(root.left)
+    print(root.value, sep=" ")
+    inOrder(root.right)
+    
+def preOrder(root):
+    if root == None:
+        return 
+    
+    print(root.value, sep=" ")
+    preOrder(root.left)
+    preOrder(root.right)
+
+
+
+# # Example usage
+# root = buildNode()
+# levelOrderTraversal(root)
+
+# newNode = node(10)
 
 # creating a tree
-buildNode()
+root = buildNode()
+# 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+
+#traversal
+levelOrderTraversal(root)
+
+print("inorder traversal")
+inOrder(root)
+
+print("psot order")
+preOrder(root)
 
